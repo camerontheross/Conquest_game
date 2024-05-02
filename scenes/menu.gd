1c1dab95faad6a506146001d737da884f5c826a6
Cameron Ross <camerontheross@gmail.com>
2024-05-02 19:17:51 -0400

    added gun model, and crosshair both as placeholders

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -4,7 +4,7 @@ func _ready():
 	$VBoxContainer/StartButton.grab_focus()
 
 func _on_start_button_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 
 func _on_options_button_pressed():
